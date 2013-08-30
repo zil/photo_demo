e5174eabc786aa9cfd57bd23c4aa6f3c43c7735e
Vacker <fantanstic@gmail.com>
2013-08-31 00:21:36 +0800

    remove edit div

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,9 +1,7 @@
 $(function(){
     $('table.frame img').on('click',function(){
 	var bgStyle = "url("+$(this).prop("src")+") no-repeat center center";
-	$('div.big-frame').css("background",bgStyle);
-	$.get("/photos/"+$(this).prop('id') + '/edit').done(function(d){
-	    $('div.des').html(d);
-	});
+	$('div.big-frame').css("background",bgStyle)
+			  .prop("title",$(this).prop("alt"));
     }).eq(0).trigger("click");
 })
